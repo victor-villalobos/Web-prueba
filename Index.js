a5733367c07afe71572ce560e466d995482a3298
sumbido <92985895+sumbido@users.noreply.github.com>
2021-11-09 11:04:52 +0100

    Modificaciones de la plantilla principal

--- Index.js
+++ Index.js
@@ -19,6 +19,10 @@ app.set("view engine", "handlebars")
 // Ahora debemos de habilitar o abrir un puerto para la aplicación que hemos creado. Para ello vamos a establecer una variable de entorno con una condicional por si no existiese ninguna.
 const port = process.env.PORT || 3000
 
+// Creamos una función de autentificación
+function isAuthenticated(user, password) {
+  return user == 'admin' && password == 'admin'
+}
 // Ahora vamos a crear la acción que ocurre cuando alguien entra en la web. Cuando alguien accede a la web hace un get y entonces se le transmite la información.
 app.get("/", function(request, response){
   //response.send("Hola, esta es mi web con nodemon.")
@@ -39,6 +43,10 @@ app.get("/login", function(request, response){
   response.render("login")
 })
 
+app.get("/dashboard", function(request, response){ // Creamos un dashboard para los usuarios admitidos o confirmados.
+  // Nos permite recoger los datos de usuarios establecidos .
+  response.render("dashboard")
+})
 
 //vamos a crear una respuesta de formulario o solicitud
 app.post("/hola", function(request, response){
@@ -58,18 +66,19 @@ app.post("/contacto", function(request, response){
 app.post("/login", function(request, response){
   const user = request.body.user
   const password = request.body.password
-  if (checkAuthentication(user, password)){
-    response.send("Datos correctos. ¡Exito!")
+  if (isAuthenticated(user, password)) {
+    //response.send("Datos correctos. ¡Exito!")
     response.redirect("/dashboard")
   } else {
-    response.send("Alguno de los datos introducidos es incorrecto. ¡error!")
+    //response.send("Alguno de los datos introducidos es incorrecto. ¡error!")
+    response.render("login", {message: "Alguno de los datos introducidos es incorrecto. ¡Error!"})
   }
   
 })
 
-// Vamos a crear una variable en la URL web para que nos lleve a la página deseada,. Es una consulta para traerme los datos solicitados.
+// Vamos a crear una variable en la URL web para que nos lleve a la página deseada,. Es una consulta para traerme los datos solicitados. Habitualmente se indica una categoría principal donde s agrupan esos datos.
 
-app.get('/:user', function(request, response){
+app.get('/users/:user', function(request, response){
   response.send(`Usuario ${request.params.user}`)
 })
 
